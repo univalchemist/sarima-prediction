dfb6222971e4d373501a1b0b15671e32fa98987b
univalchemist <guix7777@outlook.com>
2021-11-26 18:12:31 +0000

    update

--- joblib_test.py
+++ joblib_test.py
@@ -6,14 +6,14 @@ from multiprocessing import cpu_count
 print('-----------parallel---------------------')
 time1 = datetime.datetime.now()
 
-# # CPU calculation
-# executor = Parallel(n_jobs=cpu_count(), backend='multiprocessing')
-# tasks = (delayed(sqrt)(i**2) for i in range(100000))
-# scores = executor(tasks)
+# CPU calculation
+#executor = Parallel(n_jobs=cpu_count(), backend='multiprocessing')
+#tasks = (delayed(sqrt)(i**2) for i in range(100000))
+#scores = executor(tasks)
 
 # Normal calculation
 for i in range(100000):
-    print(sqrt(i**2))
+   print(sqrt(i**2))
 
 time2 = datetime.datetime.now()
 
